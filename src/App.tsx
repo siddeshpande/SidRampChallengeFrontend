abb036be56778d6eeb34bbd09c3ac59a30f9956b
siddeshpande <68968218+siddeshpande@users.noreply.github.com>
2023-09-05 19:24:14 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { InputSelect } from "./components/InputSelect"
+import { Instructions } from "./components/Instructions"
+import { Transactions } from "./components/Transactions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+import { Employee } from "./utils/types"
+
+export function App() {
+  const { data: employees, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const transactions = useMemo(() => paginatedTransactions?.data ?? transactionsByEmployee ?? null, [
+    paginatedTransactions,
+    transactionsByEmployee,
+  ])
+
+  const loadAllTransactions = useCallback(async () => {
+    setIsLoading(true)
+    // transactionsByEmployeeUtils.invalidateData()
+    //Bug 4: Clicking on View More button not showing correct data
+
+    await employeeUtils.fetchAll()
+    setIsLoading(false)
+    //Bug 5: Employees filter not available during loading more data
+
+    await paginatedTransactionsUtils.fetchAll()
+  }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+
+  const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string) => {
+      paginatedTransactionsUtils.invalidateData()
+      await transactionsByEmployeeUtils.fetchById(employeeId)
+    },
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils, transactions]
+  )
+
+  useEffect(() => {
+    if (employees === null && !employeeUtils.loading) {
+      loadAllTransactions()
+    }
+  }, [employeeUtils.loading, employees, loadAllTransactions, transactions])
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="RampBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={isLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+            if (newValue === null) {
+              return
+            }
+            //Bug 3: Cannot select _All Employees_ after selecting an employee
+            //When the selected value's id is an empty string (which corresponds to "All Employees"), 
+            //it calls the loadAllTransactions function to load all transactions.
+            newValue.id !== ""
+              ? await loadTransactionsByEmployee(newValue.id)
+              : await loadAllTransactions()
+          }}
+        />
+
+        <div className="RampBreak--l" />
+
+        <div className="RampGrid">
+          <Transactions transactions={transactions} />
+
+          {transactions !== null && (
+            <button
+              className="RampButton"
+              disabled={
+                paginatedTransactionsUtils.loading || paginatedTransactions?.nextPage == null
+                  ? true
+                  : false
+              }
+              onClick={async () => {
+                await loadAllTransactions()
+              }}
+            >
+              View More
+
+            </button>
+          )}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
